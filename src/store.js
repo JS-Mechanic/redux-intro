3d18594a712699325521c9f217f58c67294dc633
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-11 14:27:10 +0330

    Implementing action creator functions

--- src/store.js
+++ src/store.js
@@ -27,12 +27,31 @@ function reducer(state = initialState, action) {
 	}
 }
 
+// Action Creator functions:
+function deposit(amount) {
+	return {type: "account/deposit", payload: amount};
+}
+
+function withdrawal(amount) {
+	return {type: "account/withdrawal", payload: amount};
+}
+
+function requestLoan(amount, purpose) {
+	return {type: "account/requestLoan", payload: {amount: amount, purpose: purpose}};
+}
+
+function payLoan() {
+	return {type: "account/payLoan"};
+}
+
 const store = createStore(reducer);
-store.dispatch({type: "account/deposit", payload: 500});
+store.dispatch(deposit(500));
+
 console.log(store.getState());
-store.dispatch({type: "account/withdrawal", payload: 200});
+store.dispatch(withdrawal(200));
+
 console.log(store.getState());
-store.dispatch({type: "account/requestLoan", payload: {amount: 1000, purpose: "Buy a car"}});
+store.dispatch(requestLoan(2000, "Buy a macbook"));
 console.log(store.getState());
-store.dispatch({type: "account/payLoan"});
+store.dispatch(payLoan());
 console.log(store.getState());

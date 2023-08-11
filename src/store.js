b626c0e8f2812372204770ef4b2dbf970f167c6b
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-11 13:16:53 +0330

    Importing relevant modules and fixing reducer function

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import {createStore} from "redux";
+
 const initialState = {
 	balance: 0,
 	loan: 0,
@@ -12,7 +14,12 @@ function reducer(state = initialState, action) {
 			return {...state, balance: state.balance - action.payload};
 		case "account/requestLoan":
 			if (state.loan > 0) return state;
-			return {...state, loan: action.payload};
+			return {
+				...state,
+				loan: action.payload.amount,
+				loanPurpose: action.payload.purpose,
+				balance: state.balance + action.payload.amount,
+			};
 		case "account/payLoan":
 			return {...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan};
 		default:

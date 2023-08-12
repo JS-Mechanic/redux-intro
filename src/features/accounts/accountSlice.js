cac59e944168bbaba8b98ae0b40e98e11e53a730
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-12 12:14:14 +0330

    Locating all account related stuff into accountSlice.js

---
+++ src/features/accounts/accountSlice.js
@@ -0,0 +1,45 @@
+// Account initial state
+const initialStateAccount = {
+	balance: 0,
+	loan: 0,
+	loanPurpose: "",
+};
+
+// Account reducer function
+export default function AccountReducer(state = initialStateAccount, action) {
+	switch (action.type) {
+		case "account/deposit":
+			return {...state, balance: state.balance + action.payload};
+		case "account/withdrawal":
+			return {...state, balance: state.balance - action.payload};
+		case "account/requestLoan":
+			if (state.loan > 0) return state;
+			return {
+				...state,
+				loan: action.payload.amount,
+				loanPurpose: action.payload.purpose,
+				balance: state.balance + action.payload.amount,
+			};
+		case "account/payLoan":
+			return {...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan};
+		default:
+			return state;
+	}
+}
+
+// Account action creator functions
+export function deposit(amount) {
+	return {type: "account/deposit", payload: amount};
+}
+
+export function withdrawal(amount) {
+	return {type: "account/withdrawal", payload: amount};
+}
+
+export function requestLoan(amount, purpose) {
+	return {type: "account/requestLoan", payload: {amount: amount, purpose: purpose}};
+}
+
+export function payLoan() {
+	return {type: "account/payLoan"};
+}

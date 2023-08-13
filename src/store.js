8d2403a87c5662f7c2e14029b8214d0aef890e76
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-13 11:39:22 +0330

    Installing and using redux devtools

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
 import AccountReducer from "./features/accounts/accountSlice.js";
 import customerReducer from "./features/customers/customerSlice.js";
 
 const rootReducer = combineReducers({account: AccountReducer, customer: customerReducer});
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
